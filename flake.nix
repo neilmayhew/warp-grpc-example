3f78203ad5cad589f5f0fcbbbd0d956dbb6551d9
Neil Mayhew <neil.mayhew@iohk.io>
2023-11-17 15:51:05 -0700

    Add flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "warp-grpc-example";
+
+  inputs = {
+
+    hackage = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.hackage.follows = "hackage";
+    };
+
+    nixpkgs.follows = "haskell-nix/nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { flake-utils, nixpkgs, haskell-nix, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      let
+        warp-grpc-example-overlay = final: prev: {
+          warp-grpc-example = final.haskell-nix.cabalProject' rec {
+            src = final.haskell-nix.cleanSourceHaskell {
+              src = ./.;
+              name = "warp-grpc-example-src";
+            };
+            compiler-nix-name = "ghc928";
+            shell.buildInputs = with final; [
+              haskell.packages.${compiler-nix-name}.proto-lens-protoc
+            ];
+          };
+        };
+        overlays = [ haskell-nix.overlay warp-grpc-example-overlay ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskell-nix) config; };
+        flake = pkgs.warp-grpc-example.flake { };
+      in
+      flake // {
+        packages.default = flake.packages."warp-grpc-example:exe:warp-grpc-example";
+      });
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    allow-import-from-derivation = true;
+    accept-flake-config = true;
+  };
+}
